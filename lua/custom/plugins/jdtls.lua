eb765671db0d1574a78176ba1038d6b56d3935fe
Jona Löffler <>
2024-06-12 10:36:23 +0200

    Update

--- lua/custom/plugins/jdtls.lua
+++ lua/custom/plugins/jdtls.lua
@@ -22,7 +22,7 @@ return {
         local jdtls_path = vim.fn.stdpath 'data' .. '/mason/packages/jdtls'
         local path_to_lsp_server = jdtls_path .. '/config_linux'
         local path_to_plugins = jdtls_path .. '/plugins/'
-        local path_to_jar = path_to_plugins .. 'org.eclipse.equinox.launcher_1.6.800.v20240304-1850.jar'
+        local path_to_jar = path_to_plugins .. 'org.eclipse.equinox.launcher_1.6.800.v20240330-1250.jar'
         local lombok_path = jdtls_path .. '/lombok.jar'
 
         -- local root_markers = { ".git", "mvnw", "gradlew", "pom.xml", "build.gradle" }
--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -1,4 +1,66 @@
+local keymaps = function()
+  vim.keymap.set('n', '<leader>tp', function()
+    require('neotest').run.run(vim.uv.cwd())
+  end, { desc = '[T]est [P]project', noremap = true })
+
+  vim.keymap.set('n', '<leader>tf', function()
+    require('neotest').run.run(vim.fn.expand '%')
+  end, { desc = '[T]est [F]ile', noremap = true })
+
+  vim.keymap.set('n', '<leader>tr', function()
+    require('neotest').run.run()
+  end, { desc = '[T]est [R]un', noremap = true })
+
+  vim.keymap.set('n', '<leader>tS', function()
+    require('neotest').run.stop()
+  end, { desc = '[T]est [S]top', noremap = true })
+
+  vim.keymap.set('n', '<leader>ta', function()
+    require('neotest').run.attach()
+  end, { desc = '[T]est [A]ttach', noremap = true })
+
+  vim.keymap.set('n', '<leader>ts', function()
+    require('neotest').summary.open()
+  end, { desc = '[T]est [S]ummary', noremap = true })
+
+  vim.keymap.set('n', '<leader>to', function()
+    require('neotest').output.open()
+  end, { desc = '[T]est output [O]pen', noremap = true })
+
+  vim.keymap.set('n', '<leader>tO', function()
+    require('neotest').output_panel.open()
+  end, { desc = '[T]est output panel [O]pen', noremap = true })
+end
+
 return {
+  {
+    'marilari88/neotest-vitest',
+    ft = 'typescript',
+    dependencies = {
+      'nvim-neotest/neotest',
+      dependencies = {
+        'nvim-neotest/nvim-nio',
+        'nvim-lua/plenary.nvim',
+        'antoinemadec/FixCursorHold.nvim',
+        'nvim-treesitter/nvim-treesitter',
+      },
+    },
+    config = function()
+      require('neotest').setup {
+        adapters = {
+          require 'neotest-vitest',
+        },
+        output = { open_on_run = true },
+        quickfix = {
+          open = function()
+            vim.cmd 'copen'
+          end,
+        },
+      }
+
+      keymaps()
+    end,
+  },
   {
     'rcasia/neotest-java',
     tag = 'v0.10.0',
@@ -27,37 +89,7 @@ return {
         },
       }
 
-      vim.keymap.set('n', '<leader>tp', function()
-        require('neotest').run.run(vim.uv.cwd())
-      end, { desc = '[T]est [P]project', noremap = true })
-
-      vim.keymap.set('n', '<leader>tf', function()
-        require('neotest').run.run(vim.fn.expand '%')
-      end, { desc = '[T]est [F]ile', noremap = true })
-
-      vim.keymap.set('n', '<leader>tr', function()
-        require('neotest').run.run()
-      end, { desc = '[T]est [R]un', noremap = true })
-
-      vim.keymap.set('n', '<leader>tS', function()
-        require('neotest').run.stop()
-      end, { desc = '[T]est [S]top', noremap = true })
-
-      vim.keymap.set('n', '<leader>ta', function()
-        require('neotest').run.attach()
-      end, { desc = '[T]est [A]ttach', noremap = true })
-
-      vim.keymap.set('n', '<leader>ts', function()
-        require('neotest').summary.open()
-      end, { desc = '[T]est [S]ummary', noremap = true })
-
-      vim.keymap.set('n', '<leader>to', function()
-        require('neotest').output.open()
-      end, { desc = '[T]est output [O]pen', noremap = true })
-
-      vim.keymap.set('n', '<leader>tO', function()
-        require('neotest').output_panel.open()
-      end, { desc = '[T]est output panel [O]pen', noremap = true })
+      keymaps()
     end,
   },
 }
